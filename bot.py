228752913c00a1f637950d8d6c1f7cd19ac4b80a
n.vinnikov <n.vinnikov@mercuryo.io>
2021-10-04 23:04:10 +0300

    add functions to simple telegram bot

--- bot.py
+++ bot.py
@@ -1,45 +1,43 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types.message import ContentType
 from aiogram.utils import executor
+from aiogram.utils.markdown import text, bold, italic, code, pre
+
+from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions
 
 from config import TOKEN
-
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Hello!\nIt is cryptobot, write coin symbol or --h/help")
+    msg = text(bold('Hello, I am cryptobot, write coin symbol or:'),
+               '/help', sep='\n')
+    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
-@dp.message_handler(commands=['help', '--h'])
+
+@dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply('Write coin symbol or:\n(--h) - help\n(--q) - quit\n(--x) - example\n(--a) - all coins')
+    msg = text(bold('Write coin symbol or:'),
+               '/help', '/example', '/group', '/all', sep='\n')
+    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.message_handler(content_types=ContentType.ANY)
+async def unknown_message(msg: types.Message):
+    message_text = text(('Я не знаю такой команды'),
+                        italic('\nПопробуй,'), 'использовать',
+                        code('команду'), '/help')
+    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
+
 if __name__ == '__main__':
     executor.start_polling(dp)
-
-
-# while True:
-#     x = input()
-#     qq = Cryptocurrency()
-#     all = qq.get_all_symbols()
-#     if x == '--h':
-#         print('Write coin symbol or:\n(--h) - help\n(--q) - quit\n(--x) - example\n(--a) - all coins')
-#         continue
-#     if x == '--x':
-#         print(qq.get_20_coins())
-#         print('Example:\nBTC\nBitcoin (BTC) is a cryptocurrency\nBitcoin is 48,086.15609069 USD\nBTC is down -0.04 over the last 24 hours.')
-#     if x == '--a':
-#         print(all)
-#     if x in all:
-#         print(qq.get_coin_description_by_symbol(x))
-#         print(qq.get_market_price(x))
-#         print(qq.get_market_statistic(x))
-#         continue
-#     if x == '--q':
-#         exit()
